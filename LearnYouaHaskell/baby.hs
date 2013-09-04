025626602b2b619adf0f93ffc1fd50166feeea1b
root <root@archy.cds-lab>
2013-09-04 08:11:59 -0400

    At least to chapter 3

--- LearnYouaHaskell/baby.hs
+++ LearnYouaHaskell/baby.hs
@@ -21,3 +21,26 @@ length' xs = sum [ 1 | _ <- xs]
 removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z']]
 
 rightTriangles' = [ (a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10], a^2 + b^2 == c^2, a+b+c == 24, a < b]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double 
+circumference' r = 2 * pi * r
+
+--  Chapter 3
+
+lucky :: Int -> String
+lucky 7 = "Lucky Number Seven!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
