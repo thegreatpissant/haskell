a2411ec5ab89d1db3dd6413c5c6cd3ff5830bfc7
thegreatpissant <thegreatpissant@gmail.com>
2013-09-04 22:53:02 -0400

    more of tutorial 3

--- LearnYouaHaskell/baby.hs
+++ LearnYouaHaskell/baby.hs
@@ -44,3 +44,40 @@ sayMe 3 = "Three!"
 sayMe 4 = "Four!"
 sayMe 5 = "Five!"
 sayMe x = "Not between 1 and 5"
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Bonnie"
+charName 'c' = "Chandler"
+charName 'd' = "Danielle"
+charName  x  = "YourName"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+-- addVectors a b = (fst a + fst b, snd a + snd b)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+
+head' :: [a] -> a
+head' [] = error "empty headed"
+head' (x:_) = x
+
+abbyNormal :: [a] -> a
+abbyNormal (xs) = head' xs
+
+add' :: Int -> Int -> Int
+add' a b = a + b
+
+tell :: (Show  a ) => [a] -> String
+tell [] = "The List is Empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long, the first two elements are: " ++ show x ++ " and " ++ show y
